7c0f9f2177dbcee936a73b23d9318cb447be6f50
Maria Júlia (facul) <maria.santos.1451067@sga.pucminas.br>
2024-06-09 21:05:53 -0300

    repository created and updating with the base code

---
+++ script.js
@@ -0,0 +1,45 @@
+function searchMovie() {
+    const apiKey = '27b8aba';
+    const movieTitle = $('#movie-search').val();
+    console.log(movieTitle)
+
+    if (movieTitle.trim() !== '') {
+        $.ajax({
+            url: `https://www.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}`,
+            method: 'GET',
+            success: function (data) {
+                console.log(data);
+                displayMovieDetails(data)
+            },
+            error: function (error) {
+                displayError('Filme não encontrado. Verifique o título e tente novamente.');
+            }
+        });
+    } else {
+        displayError('Por favor, insira o nome do filme.');
+    }
+}
+
+function displayMovieDetails(movie) {
+    const movieDetailsContainer = $('#movie-details');
+    movieDetailsContainer.empty();
+
+    if (movie.Response === 'True') {
+        movieDetailsContainer.append(`
+            <h2>${movie.Title}</h2>
+            <p><strong>Ano:</strong> ${movie.Year}</p>
+            <p><strong>Classificação:</strong> ${movie.Rated}</p>
+            <p><strong>Elenco:</strong> ${movie.Actors}</p>
+            <p><strong>Enredo:</strong> ${movie.Plot}</p>
+            <img src="${movie.Poster}" alt="${movie.Title}" width="200">
+        `);
+    } else {
+        console.log('Filme não encontrado!! Verifique o título e tente novamente.');
+    }
+}
+
+function displayError(message) {
+    const movieDetailsContainer = $('#movie-details');
+    movieDetailsContainer.empty();
+    movieDetailsContainer.append(`<p class="error">${message}</p>`);
+}
